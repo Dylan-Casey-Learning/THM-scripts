00513ea2ebe0199d57f61c14fe705190b171eae5
Dylan-Casey-Learning <86577633+Dylan-Casey-Learning@users.noreply.github.com>
2021-06-28 01:00:54 +0100

    Update Base64Decoder.py

--- Base64Decoder.py
+++ Base64Decoder.py
@@ -6,17 +6,17 @@ import base64
 
 # for the THM room the text file was encoded 15 times 5 each for base64,32,16
 # define three functions
-def b64d(encoded):
+def B64(encoded):
     decoded = base64.b64decode(encoded)
     return decoded
 
 
-def b32d(encoded):
+def B32(encoded):
     decoded = base64.b32decode(encoded)
     return decoded
 
 
-def b16d(encoded):
+def B16(encoded):
     decoded = base64.b16decode(encoded)
     return decoded
 
@@ -25,16 +25,16 @@ encodedfile= open('base64.txt', 'rb').read()
 
 #for loops for each base65, base32, base16
 for i in range(5):
-    encodedflag = b64d(encodedfile)
+    encodedflag = B64(encodedfile)
 
 for i in range(5):
-    encodedflag = b32d(encodedfile)
+    encodedflag = B32(encodedfile)
 
 for i in range(5):
     if i != 4:
-        encodedflag = b16d(encodedfile)
+        encodedflag = B16(encodedfile)
     else:
-        encodedflag = b16d(encodedfile)
+        encodedflag = B16(encodedfile)
         print ('The flag is:', encodedfile)
 # close file
 encodedfile.close()
